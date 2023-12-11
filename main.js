9ba4cbe16e66092878c791fdfe7cb6bf15d8da4d
Vijikaran <89895231+Vijikaran-root@users.noreply.github.com>
2023-12-11 10:00:51 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  var x = document.getElementById("mob-nav-opt");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
